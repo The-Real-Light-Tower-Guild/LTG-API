a2a9a04a9efe0a31000bb8b6df6e1739d5c1afda
Wes <wesleySalesberry@hotmail.com>
2021-01-22 09:27:54 -0500

    added function to handle importing and deleteing test data

--- seeder.js
+++ seeder.js
@@ -15,3 +15,37 @@ mongoose.connect(
 )
 
 // Once the Model are created add them here
+//Grab the model
+const User = require('./models/User')
+//Read the User.json file in _data
+const user = JSON.parse(fs.readFileSync(`${__dirname}/_data/_user.json`))
+
+//Add the data into the database
+const importData = async () => {
+    try {
+        await User.create(user)
+        console.log('Data imported...'.green.inverse)
+        process.exit()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//Delete the data fromthe database
+const deleteData = async () => {
+    try {
+        await User.deleteMany()
+        console.log('Data destoryed...'.red.inverse)
+        process.exit()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+if(process.argv[2] === '-import'){
+    importData()
+}else if(process.argv[2] === '-delete'){
+    deleteData()
+
+}
+
